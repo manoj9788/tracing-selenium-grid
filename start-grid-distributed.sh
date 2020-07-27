a191b70c08038cefa61a8b22532bd4c8b8aa42cb
pujagani <puja.jagani93@gmail.com>
2020-07-27 18:56:57 +0530

    [tracing] Update jaeger exporter version to 0.6.0. Update grpc-okhttp to 1.29.0.

--- start-grid-distributed.sh
+++ start-grid-distributed.sh
@@ -5,8 +5,8 @@ java -DJAEGER_SERVICE_NAME="selenium-session-map" \
      -DJAEGER_AGENT_PORT=14250 \
      -jar selenium-server.jar \
      --ext $(coursier fetch -p \
-        io.opentelemetry:opentelemetry-exporters-jaeger:0.2.0 \
-        io.grpc:grpc-okhttp:1.26.0) \
+        io.opentelemetry:opentelemetry-exporters-jaeger:0.6.0 \
+        io.grpc:grpc-okhttp:1.29.0) \
      sessions &
     
 sleep 2
@@ -16,8 +16,8 @@ java -DJAEGER_SERVICE_NAME="selenium-distributor" \
      -DJAEGER_AGENT_PORT=14250 \
      -jar selenium-server.jar \
      --ext $(coursier fetch -p \
-        io.opentelemetry:opentelemetry-exporters-jaeger:0.2.0 \
-        io.grpc:grpc-okhttp:1.26.0) \
+        io.opentelemetry:opentelemetry-exporters-jaeger:0.6.0 \
+        io.grpc:grpc-okhttp:1.29.0) \
      distributor --sessions http://localhost:5556 &
 
 sleep 2
@@ -27,8 +27,8 @@ java -DJAEGER_SERVICE_NAME="selenium-router" \
      -DJAEGER_AGENT_PORT=14250 \
      -jar selenium-server.jar \
      --ext $(coursier fetch -p \
-        io.opentelemetry:opentelemetry-exporters-jaeger:0.2.0 \
-        io.grpc:grpc-okhttp:1.26.0) \
+        io.opentelemetry:opentelemetry-exporters-jaeger:0.6.0 \
+        io.grpc:grpc-okhttp:1.29.0) \
      router --sessions http://localhost:5556 --distributor http://localhost:5553 &
 
 sleep 2
@@ -38,7 +38,7 @@ java -DJAEGER_SERVICE_NAME="selenium-node" \
      -DJAEGER_AGENT_PORT=14250 \
      -jar selenium-server.jar \
      --ext $(coursier fetch -p \
-        io.opentelemetry:opentelemetry-exporters-jaeger:0.2.0 \
-        io.grpc:grpc-okhttp:1.26.0) \
+        io.opentelemetry:opentelemetry-exporters-jaeger:0.6.0 \
+        io.grpc:grpc-okhttp:1.29.0) \
      node -D selenium/standalone-firefox:3.141.59 '{"browserName": "firefox"}' &
      
--- start-grid-standalone.sh
+++ start-grid-standalone.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
-ps auxw | grep selenium-server-4.0.0-alpha-5.jar | awk '{print $2}' | xargs kill
+ps auxw | grep selenium-server-4.0.0-alpha-7.jar | awk '{print $2}' | xargs kill
  
 java -DJAEGER_SERVICE_NAME="selenium-standalone" \
     -DJAEGER_AGENT_HOST=localhost \
     -DJAEGER_AGENT_PORT=14250 \
     -jar selenium.jar \
     --ext $(coursier fetch -p \
-        io.opentelemetry:opentelemetry-exporters-jaeger:0.2.0 \
-        io.grpc:grpc-okhttp:1.26.0) \
+        io.opentelemetry:opentelemetry-exporters-jaeger:0.6.0 \
+        io.grpc:grpc-okhttp:1.29.0) \
     standalone
