b30abb806edacddb683b244c5d78cf1f091864cd
Puja Jagani <puja.jagani93@gmail.com>
2021-02-02 11:04:29 +0530

    Update scripts for Alpha 7

--- start-grid-standalone.sh
+++ start-grid-standalone.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
-ps auxw | grep selenium-server-4.0.0-alpha-6.jar | awk '{print $2}' | xargs kill
+ps auxw | grep selenium-server-4.0.0-alpha-7.jar | awk '{print $2}' | xargs kill
  
 java -DJAEGER_SERVICE_NAME="selenium-standalone" \
     -DJAEGER_AGENT_HOST=localhost \
     -DJAEGER_AGENT_PORT=14250 \
-    -jar selenium.jar \
+    -jar selenium-server-4.0.0-alpha-7.jar \
     --ext $(coursier fetch -p \
-        io.opentelemetry:opentelemetry-exporters-jaeger:0.6.0 \
-        io.grpc:grpc-okhttp:1.29.0) \
+        io.opentelemetry:opentelemetry-exporters-jaeger:0.9.1 \
+        io.grpc:grpc-netty:1.32.1) \
     standalone
