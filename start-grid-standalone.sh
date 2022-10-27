8919dad3ce7aaaaeb8e6877432f5cfbdab71212c
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2022-10-27 21:42:05 +0530

    Fix distributed grid shell script

--- start-grid-standalone.sh
+++ start-grid-standalone.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -x
 
-ps auxw | grep selenium-server-4.0.0-alpha-7.jar | awk '{print $2}' | xargs kill
+jps | grep selenium | awk '{print $1}' | while read line; do
+    echo 'Killing process id ' $line
+    kill -9 $line
+done
+
+SELENIUM_JAR="selenium-server-4.5.3.jar"
+JAEGAR_JVM_ARGS="-Dotel.traces.exporter=jaeger -Dotel.exporter.jaeger.endpoint=http://localhost:14250 "
+COURSIER="$(coursier fetch -p io.opentelemetry:opentelemetry-exporter-jaeger:1.19.0 io.grpc:grpc-netty:1.50.2)"
  
-java -Dotel.traces.exporter=jaeger \
-     -Dotel.exporter.jaeger.endpoint=localhost:14250 \
+java $JAEGAR_JVM_ARGS \
      -Dotel.resource.attributes=service.name=selenium-standalone \
-     -jar selenium-beta-4.jar \
-     --ext $(coursier fetch -p \
-        io.opentelemetry:opentelemetry-exporter-jaeger:1.0.0 \
-        io.grpc:grpc-netty:1.35.0) \
-    standalone
+     -jar  $SELENIUM_JAR --ext $COURSIER standalone
