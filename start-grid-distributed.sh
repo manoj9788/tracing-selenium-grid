0e5b3c3086b0bbe2524c81db275dc61691e8d4f3
Tyler Benson <tylerbenson@gmail.com>
2023-11-02 11:07:45 -0400

    Use OTLP exporter instead of Jaeger specific
    This allows much greater flexibility as Jaeger already supports OTLP, but so do other systems.
    
    I also simplified the process to not require Coursier.  Instead I use the included maven wrapper to download the exporter's transitive dependencies.
    
    Hope this is helpful.

--- start-grid-distributed.sh
+++ start-grid-distributed.sh
@@ -4,34 +4,37 @@ jps | grep selenium | awk '{print $1}' | while read line; do
     kill -9 $line
 done
 
-SELENIUM_JAR="selenium-server-4.5.3.jar"
-JAEGAR_JVM_ARGS="-Dotel.traces.exporter=jaeger -Dotel.exporter.jaeger.endpoint=http://localhost:14250 "
-COURSIER="$(coursier fetch -p io.opentelemetry:opentelemetry-exporter-jaeger:1.19.0 io.grpc:grpc-netty:1.50.2)"
-java $JAEGAR_JVM_ARGS \
+./mvnw dependency:copy-dependencies -DincludeScope=runtime
+
+SELENIUM_JAR="selenium-server.jar"
+TRACER_DEPS="./target/dependency"
+OTEL_JVM_ARGS="-Dotel.traces.exporter=otlp -Dotel.java.global-autoconfigure.enabled=true"
+
+java $OTEL_JVM_ARGS \
      -Dotel.resource.attributes=service.name=selenium-event-bus \
-     -jar  $SELENIUM_JAR --ext $COURSIER \
+     -jar  $SELENIUM_JAR --ext $TRACER_DEPS \
      event-bus &
 sleep 2
-java $JAEGAR_JVM_ARGS \
+java $OTEL_JVM_ARGS \
      -Dotel.resource.attributes=service.name=selenium-sessions \
-     -jar  $SELENIUM_JAR --ext $COURSIER \
+     -jar  $SELENIUM_JAR --ext $TRACER_DEPS \
      sessions &
 sleep 2
-java $JAEGAR_JVM_ARGS \
+java $OTEL_JVM_ARGS \
      -Dotel.resource.attributes=service.name=selenium-session-queue \
-     -jar  $SELENIUM_JAR --ext $COURSIER \
+     -jar  $SELENIUM_JAR --ext $TRACER_DEPS \
      sessionqueue &
 sleep 2
-java $JAEGAR_JVM_ARGS \
+java $OTEL_JVM_ARGS \
      -Dotel.resource.attributes=service.name=selenium-distributor \
-     -jar  $SELENIUM_JAR --ext $COURSIER \
+     -jar  $SELENIUM_JAR --ext $TRACER_DEPS \
      distributor --sessions http://localhost:5556 --sessionqueue http://localhost:5559 --bind-bus false &
 sleep 2
-java $JAEGAR_JVM_ARGS \
+java $OTEL_JVM_ARGS \
      -Dotel.resource.attributes=service.name=selenium-router \
-     -jar  $SELENIUM_JAR --ext $COURSIER \
+     -jar  $SELENIUM_JAR --ext $TRACER_DEPS \
      router --sessions http://localhost:5556 --distributor http://localhost:5553 --sessionqueue http://localhost:5559 &
 sleep 2
-java $JAEGAR_JVM_ARGS \
+java $OTEL_JVM_ARGS \
      -Dotel.resource.attributes=service.name=selenium-node \
-     -jar  $SELENIUM_JAR --ext $COURSIER node -D selenium/standalone-firefox:4.5.3-20221024 '{"browserName": "firefox"}' &
+     -jar  $SELENIUM_JAR --ext $TRACER_DEPS node -D selenium/standalone-firefox:4.5.3-20221024 '{"browserName": "firefox"}' &
